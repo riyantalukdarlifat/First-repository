00b8f3ca3d6e64a2d50e6b603324443710dab962
Riyan <riyantalukdar214@gmail.com>
2025-11-15 00:12:01 -0500

    add project

---
+++ FirstJavaProject/src/FirstPack/Firstclass.java
@@ -0,0 +1,13 @@
+package FirstPack;
+
+public class Firstclass {
+	int a=7;
+	int b=8;
+	void add() {
+		System.out.println(a+b);
+	}
+	int a1() {
+		return a-b;
+	}
+
+}
---
+++ FirstJavaProject/src/FirstPack/mainclass.java
@@ -0,0 +1,11 @@
+package FirstPack;
+
+public class mainclass {
+	public static void main(String[] args) {
+		Firstclass r=new Firstclass();
+		/* r.add(); */
+		r.a1();
+		System.out.println();
+		
+	}
+}    
